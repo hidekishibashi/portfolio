7f110b0a2a8368ab718201d55242eb5ef86dbb36
hidekishibashi <dev.hidekishibashi@gmail.com>
2020-04-12 01:09:48 +0900

    add : functions api base

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,25 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+admin.initializeApp();
+exports.sections = functions.https.onRequest((req, res) => {
+  const ref = admin.database().ref('sections');
+  ref.once("value", (data) => {
+    res.send(data);
+  });
+});
+
+exports.sections_1 = functions.https.onRequest((req, res) => {
+  const ref = admin.database().ref('sections_1');
+  ref.once("value", (data) => {
+    res.send(data);
+  });
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
