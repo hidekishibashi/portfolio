30250adaa871b5af8fa1f6fffaf3fc84eb21cde8
hidekishibashi <dev.hidekishibashi@gmail.com>
2020-04-12 20:23:21 +0900

    fix : functions api

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,29 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+
+// express and parser
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 app.use(bodyParser.json());
 
+// cors
+const cors = require('cors')({origin: true});
+app.use(cors);
+
 admin.initializeApp();
-exports.sections = functions.https.onRequest((req, res) => {
-  const ref = admin.database().ref('sections');
-  ref.once("value", (data) => {
-    res.send(data);
-  });
+app.get('/', (req, res) => {
+  const query = admin.database().ref("sections_1");
+  query.once("value").then(snapshot => {
+    return res.send(snapshot);
+  }).catch(error => {
+      // eslint-disable-next-line no-alert
+      alert(error);
+      res.status(404).send('No data available.');
+    });
 });
 
-exports.sections_1 = functions.https.onRequest((req, res) => {
-  const ref = admin.database().ref('sections_1');
-  ref.once("value", (data) => {
-    res.send(data);
-  });
-});
+exports.app = functions.https.onRequest(app);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
