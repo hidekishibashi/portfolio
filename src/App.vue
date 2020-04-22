f5eb394643ef9c207766d6f71bcfc3ef24e23573
hidekishibashi <dev.hidekishibashi@gmail.com>
2020-04-22 14:07:45 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template lang='pug'>
   #app
     img(alt='Vue logo', src='./assets/logo.png')
-    HelloWorld(msg='Welcome to Your Vue.js App')
+    HelloWorld(msg='This is develop branch')
 </template>
 
 <script>
